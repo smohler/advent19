2f5551ea42ed0d82c65146c4d4fd919efa621b13
Samuel Mohler <smohler@psu.ds.pdx.edu>
2019-12-20 03:07:40 -0800

    trying to find a better criteria for most viewed

--- Day10/MonitorStation.py
+++ Day10/MonitorStation.py
@@ -1,6 +1,7 @@
 #all the functions that help solve finding the optimal asteriod station...to be tested
 import numpy as np
 from numpy import linalg as LA
+import math
 import itertools as IT
 def turnMatrix(rawdata):
     data = open(rawdata, 'r').read().split('\n')
@@ -34,22 +35,33 @@ def TotalColinears(origin, otherpoints):
     combs = list(IT.combinations(otherpoints, 2))
     return [Colinear(origin, comb[0], comb[1]) for comb in combs]
 
+def TotalTriangle(otherpoints):
+    combs = list(IT.combinations(otherpoints, 2))
+    return [TriangleCheck(comb[0], comb[1]) for comb in combs]
+
+def TriangleCheck(p1, p2):
+    #use the triangle inequality to determine if two points are on a line relative to the origin
+    norm = lambda x: math.sqrt(x[0]**2 + x[1]**2)
+    vadd = lambda x, y: (x[0] + y[0], x[1] + y[1])
+    return norm(vadd(p1, p2)) == norm(p1) + norm(p2)
+
+def shiftOrigin(origin, otherpoints):
+    shiftedPoints = [(point[0] - origin[0], point[1] - origin[1]) for point in otherpoints]
+    return shiftedPoints
+
 def allMonitors(coordinates):
     allAsteroids = coordinates
     Colinears = []
     for anAsteroid in allAsteroids:
         otherAsteroids = [x for x in allAsteroids if x != anAsteroid]
-        T = TotalColinears(anAsteroid, otherAsteroids)
+        shiftedAsteroids = shiftOrigin(anAsteroid, otherAsteroids)
+        T = TotalTriangle(shiftedAsteroids)
         Colinears.append(sum(T))
     zeros = [i for i, p  in enumerate(Colinears) if p == min(Colinears)]
     bestAsteroids = [allAsteroids[i] for i in zeros]
     #reverse entries to match problem set up. 
     flip = lambda x: x[::-1]
     bestAsteroids = list(map(flip, bestAsteroids))
-    return bestAsteroids
+    return (bestAsteroids, len(Colinears) - min(Colinears))
 
 #best canditates are ones that at least have no colinear points.
-
-
-
-    
---
+++ Day10/testMonitorStation.py
@@ -0,0 +1,31 @@
+import MonitorStation as MS
+
+#example1
+M = MS.turnMatrix('ex1.txt')
+C = MS.coordinate(M)
+stations = MS.allMonitors(C)
+print(stations)
+
+#example2
+M = MS.turnMatrix('ex2.txt')
+C = MS.coordinate(M)
+stations = MS.allMonitors(C)
+print(stations)
+
+#example3
+M = MS.turnMatrix('ex3.txt')
+C = MS.coordinate(M)
+stations = MS.allMonitors(C)
+print(stations)
+
+#example4
+M = MS.turnMatrix('ex4.txt')
+C = MS.coordinate(M)
+stations = MS.allMonitors(C)
+print(stations)
+
+#example5
+M = MS.turnMatrix('ex5.txt')
+C = MS.coordinate(M)
+stations = MS.allMonitors(C)
+print(stations)
